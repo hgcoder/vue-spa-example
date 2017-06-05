8cd8d1cc9839d8aa35acf63b780f4409c3afd47a
hgcoder <651841877@qq.com>
2017-06-05 22:28:53 +0800

    ep build

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # vue-spa-example
+
+> A Vue.js project
+
 基于vue2.x完成的单页面应用实例
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
--- config/index.js
+++ config/index.js
@@ -8,16 +8,16 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),//打包输出文件设置
     assetsSubDirectory: 'static', //打包生成的index.html中 访问路径配置 二级目录
     assetsPublicPath: '/',//index.html 上引入静态资源配置 结合线上环境挂载点设置
-    productionSourceMap: true,
+    productionSourceMap: false,
     productionGzip: false,
     title: 'vue-spa-example',
-    favicon: path.resolve(__dirname,'../static/favicon.ico'),
+    // favicon: path.resolve(__dirname,'../static/favicon.ico'),
     productionGzipExtensions: ['js', 'css'],
     bundleAnalyzerReport: process.env.npm_config_report
   },
   dev: {
     env: require('./dev.env'),
-    port: 8082,
+    port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
@@ -25,8 +25,7 @@ module.exports = {
     // favicon: path.resolve(__dirname,'../static/favicon.ico'),
     proxyTable: {
       '/task':{
-        target:'http://10.27.97.65:8080/dtp-web/task/',
-        // target:'http://10.24.61.76:8080/dtp-web/task/',
+        target:'',
         changeOrigin:true,
         pathRewrite:{ //匹配转发
           '^/task':''
